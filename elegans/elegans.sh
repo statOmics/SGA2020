ad071208efb7e0ffe0e021f1bda9de6669edb267
statOmics <lieven.clement@ugent.be>
2020-11-06 07:44:42 +0100

    rnaseq-data

---
+++ elegans/elegans.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR153/009/SRR1532959/SRR1532959.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR153/000/SRR1532960/SRR1532960.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR153/001/SRR1532961/SRR1532961.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR153/002/SRR1532962/SRR1532962.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR153/003/SRR1532963/SRR1532963.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR153/004/SRR1532964/SRR1532964.fastq.gz
---
+++ elegans/elegansFastqDump.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+~/Downloads/sratoolkit.2.5.4-mac64/bin/fastq-dump SRR1532959.sra
+~/Downloads/sratoolkit.2.5.4-mac64/bin/fastq-dump SRR1532960.sra
+~/Downloads/sratoolkit.2.5.4-mac64/bin/fastq-dump SRR1532961.sra
+~/Downloads/sratoolkit.2.5.4-mac64/bin/fastq-dump SRR1532962.sra
+~/Downloads/sratoolkit.2.5.4-mac64/bin/fastq-dump SRR1532963.sra
+~/Downloads/sratoolkit.2.5.4-mac64/bin/fastq-dump SRR1532964.sra
+
